f90076acc4f6704cfb2144bdf1d0b3e8b559718d
Calel95 <borgescalel@gmail.com>
2024-09-18 14:17:22 -0300

    ajustado o path para criacao de arquivos

--- src/create_faker_data.py
+++ src/create_faker_data.py
@@ -2,6 +2,8 @@ from faker import Faker
 import json
 from tqdm import tqdm 
 import csv
+import os
+import re
 
 faker = Faker()
 Faker.seed(0)
@@ -25,21 +27,25 @@ class Gerador:
             self.lista.append(register)
 
     def output_csv(self, file_name: str):
-        file_path = f"C:/Users/Calel/OneDrive/Área de Trabalho/Python/sistema_ETL/data/csv/{file_name}.csv"
-        with open (file_path, "w", newline='') as csv_file:
+        file_path_old = os.getcwd()
+        file_path = file_path_old.replace("src","data/csv/").replace('\\','/')
+        file_path_new = file_path+file_name+".csv"
+        with open (file_path_new, "w", newline='') as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=["ID", "Name", "created_at", "Salary"], delimiter=";")
             writer.writeheader()
             for row in self.lista:
                 writer.writerow(row)
 
-        print(f"Archive genetored in {file_path}")
+        print(f"Archive genetored in {file_path_new}")
 
     def output_json(self, file_name: str):
-        file_path = f"C:/Users/Calel/OneDrive/Área de Trabalho/Python/sistema_ETL/data/json/{file_name}.json"
-        with open (file_path, "w") as json_file:
+        file_path_old = os.getcwd()
+        file_path = file_path_old.replace("src","data/json/").replace('\\','/')
+        file_path_new = file_path+file_name+".json"
+        with open (file_path_new, "w") as json_file:
             json.dump(self.lista,json_file, indent=4, ensure_ascii=False)
 
-        print(f"Archive genetored in {file_path}")
+        print(f"Archive genetored in {file_path_new}")
 
 
 
--- src/main.py
+++ src/main.py
@@ -2,26 +2,13 @@ import duckdb
 import os
 from etl_json import DuckdbETL
 from create_faker_data import Gerador
+import re
 
 
-# os.chdir('..')
-
-
-# a = "json1.json"
-# input_one_json = duckdb.read_json(f"data/json/{a}")
-# duckdb.sql("select * from input_one_json").show()
-
-# df_one_json = DuckdbETL()
-# df_one_json.carrega_one_json("json1")
-# df_one_json.select_table()
-
-# df_all_jsons = DuckdbETL()
-# df_all_jsons.carrega_all_jsons()
-#df_all_jsons.select_table()
-
-arquivo = Gerador()
-arquivo.generator_of_registers(10000)
-arquivo.output_csv("teste1")
+a = Gerador()
+a.generator_of_registers(1000)
+a.output_csv("testando_csv")
+a.output_json("testando_json")
 
 # etl = DuckdbETL()
 # etl.json_input_one("tst")
