0fafe8d1657dd2a09724805c1508063fdf7bb0a4
Calel95 <borgescalel@gmail.com>
2024-10-22 20:11:56 -0300

    adicionando openai para difinir a funcao para o nome do campo

--- src/teste.py
+++ src/teste.py
@@ -1,11 +1,21 @@
-import pytest
-from faker import Faker
-import json
+import openai
+from dotenv import load_dotenv, find_dotenv
 
-faker = Faker()
+_ = load_dotenv(find_dotenv())
 
-def tests():
-    pass
+client = openai.Client()
 
+mensagens = [
+    {'role': 'system', 'content': 'Você precisa classificar o tipo da funcao da biblioteca Faker da palavra que for passada. Diga apenas o tipo em uma palavra, e nada mais'},
+    {'role': 'user', 'content': 'nome'}
+]
 
+resposta = client.chat.completions.create(
+    model='gpt-3.5-turbo',
+    messages=mensagens,
+    max_tokens=50,
+    temperature=0.7,
+)
 
+mensagem_resp = resposta.choices[0].message.content
+print(mensagem_resp)
