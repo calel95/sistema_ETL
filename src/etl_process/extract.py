631eb1ea5cd9429e6cacbf3fc675cec4c882305f
Calel95 <borgescalel@gmail.com>
2024-11-18 20:03:30 -0300

    versao web 0.1

--- src/etl_process/extract.py
+++ src/etl_process/extract.py
@@ -6,47 +6,47 @@ class Extract:
     def __init__(self) -> None:
         #self.file_path = file_path
         self.df = None
-        os.chdir('..')
+    #     os.chdir('..')
 
-    def one_input_json(self, arquive_name: str):
-        '''apenas o nome do arquivo json'''
-        #os.chdir('..')
-        file_path = f"data/json/{arquive_name}.json"
-        self.df = duckdb.read_json(f"data/json/{arquive_name}.json")
-        size = os.path.getsize(file_path)
-        print(f"Size do arquivo '{arquive_name}': {size} bytes")
-        return self.df
+    # def one_input_json(self, arquive_name: str):
+    #     '''apenas o nome do arquivo json'''
+    #     #os.chdir('..')
+    #     file_path = f"data/json/{arquive_name}.json"
+    #     self.df = duckdb.read_json(f"data/json/{arquive_name}.json")
+    #     size = os.path.getsize(file_path)
+    #     print(f"Size do arquivo '{arquive_name}': {size} bytes")
+    #     return self.df
     
-    def one_input_csv(self, arquive_name: str):
-        '''apenas o nome do arquivo csv'''
-        #os.chdir('..')
-        #file_path = f"data/csv/{arquive_name}.csv"
-        file_path = f"data/csv/{arquive_name}.csv"
-        self.df = duckdb.read_csv(f"data/csv/{arquive_name}.csv")
-        size = os.path.getsize(file_path)
-        print(f"Size do arquivo '{arquive_name}': {size} bytes")
-        return self.df
+    # def one_input_csv(self, arquive_name: str):
+    #     '''apenas o nome do arquivo csv'''
+    #     #os.chdir('..')
+    #     #file_path = f"data/csv/{arquive_name}.csv"
+    #     file_path = f"data/csv/{arquive_name}.csv"
+    #     self.df = duckdb.read_csv(f"data/csv/{arquive_name}.csv")
+    #     size = os.path.getsize(file_path)
+    #     print(f"Size do arquivo '{arquive_name}': {size} bytes")
+    #     return self.df
     
-    def all_input_csv(self):
-        self.df = duckdb.read_csv("data/csv/*.csv")
-        return self.df
+    # def all_input_csv(self):
+    #     self.df = duckdb.read_csv("data/csv/*.csv")
+    #     return self.df
     
-    def all_input_json(self):
-        self.df = duckdb.read_json("data/json/*.json")
-        return self.df
-
-
-    # def one_input_csv(self, file):
-    #     '''Carrega dados de um arquivo CSV em memória'''
-    #     # self.df = pd.read_csv(file)  # Usa pandas para ler diretamente do objeto de arquivo
-    #     # size = file.size  # Obtem o tamanho do arquivo
-    #     # print(f"Size do arquivo: {size} bytes")
-    #     # return self.df
-    #     self.df = duckdb.read_csv(file)
+    # def all_input_json(self):
+    #     self.df = duckdb.read_json("data/json/*.json")
     #     return self.df
 
 
-    # def one_input_json(self, file):
-    #     '''Carrega dados de um arquivo JSON em memória'''
-    #     self.df = duckdb.read_csv(file)  # Usa pandas para ler diretamente do objeto de arquivo
-    #     return self.df
+    def one_input_csv(self, file):
+        '''Carrega dados de um arquivo CSV em memória'''
+        # self.df = pd.read_csv(file)  # Usa pandas para ler diretamente do objeto de arquivo
+        # size = file.size  # Obtem o tamanho do arquivo
+        # print(f"Size do arquivo: {size} bytes")
+        # return self.df
+        self.df = duckdb.read_csv(file)
+        return self.df
+
+
+    def one_input_json(self, file):
+        '''Carrega dados de um arquivo JSON em memória'''
+        self.df = duckdb.read_csv(file)  # Usa pandas para ler diretamente do objeto de arquivo
+        return self.df
--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,8 +1,6 @@
 import sys
 import os
-
-# Adicionar o diretório src ao sistema de caminhos do Python
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+import pandas as pd
 
 import streamlit as st
 from src.etl_process.extract import Extract
@@ -29,4 +27,25 @@ elif file_type == "JSON":
     json_file = st.file_uploader("Carregar arquivo JSON", type=["json"])
     if json_file:
         df = extractor.one_input_json(json_file)
+        st.write(df)
+
+st.header("Transformar Dados")
+
+if 'df' in locals():  # Verifica se o DataFrame foi carregado
+    transform = Transform(df)
+
+    if st.button("Remover Duplicatas"):
+        df = transform.remove_data_duplicates()
+        st.write(df)
+
+    if st.button("Remover Nulos"):
+        columns_to_check = st.text_input("Colunas para verificar nulos (separadas por vírgula):")
+        if columns_to_check:
+            columns_list = [col.strip() for col in columns_to_check.split(",")]
+            df = transform.remove_data_nulls(columns_list)
+            st.write(df)
+
+    query = st.text_area("Consulta SQL para filtrar dados:")
+    if st.button("Filtrar Dados"):
+        df = transform.filter_select(query)
         st.write(df)
